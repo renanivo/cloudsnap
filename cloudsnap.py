1e18c3296b9ba931a701ad75c746bba367a672f4
Renan Ivo <renanivom@gmail.com>
2011-11-07 22:31:03 -0200

    initial commit - dummy instance listing

---
+++ cloudsnap.py
@@ -0,0 +1,17 @@
+import webapp2
+from boto.ec2.connection import EC2Connection
+
+from settings import AWS
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        connection = EC2Connection(AWS['key'],
+                                   AWS['secret'],
+                                   is_secure=False)
+
+        instances = connection.get_all_instances()
+        self.response.out.write(instances)
+
+app = webapp2.WSGIApplication([('/', MainPage)],
+                              debug=True)
