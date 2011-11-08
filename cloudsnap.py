d544c44f07134db3c0628eebf50a23c70ddf303e
Renan Ivo <renanivom@gmail.com>
2011-11-08 16:42:37 -0200

    create images of every ec2 instance

--- cloudsnap.py
+++ cloudsnap.py
@@ -1,3 +1,5 @@
+import datetime
+
 import webapp2
 from boto.ec2.connection import EC2Connection
 
@@ -6,12 +8,15 @@ from settings import *
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        connection = EC2Connection(AWS['key'],
-                                   AWS['secret'],
-                                   is_secure=SAFE)
+        c = EC2Connection(AWS['key'], AWS['secret'], is_secure=SAFE)
 
-        instances = connection.get_all_instances()
-        self.response.out.write(instances)
+        for reservation in c.get_all_instances():
+            for instance in reservation.instances:
+                name = "%s-%s" % (datetime.date.today(), instance.id)
+                image_id = c.create_image(instance.id, name)
+
+                message = "%s backed up on image %s" % (instance.id, image_id)
+                self.response.out.write(message)
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
