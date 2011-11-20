12c9cbcac644e243d1d724190447180a903953d3
Renan Ivo <renanivom@gmail.com>
2011-11-20 15:59:33 -0200

    rename MainPage controller to BackupPage and put it in a new file

--- cloudsnap.py
+++ cloudsnap.py
@@ -1,22 +1,6 @@
-import datetime
-
 import webapp2
-from boto.ec2.connection import EC2Connection
 
-from settings import *
+from controllers import *
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        c = EC2Connection(AWS['key'], AWS['secret'], is_secure=SAFE)
-
-        for reservation in c.get_all_instances():
-            for instance in reservation.instances:
-                name = "%s-%s" % (datetime.date.today(), instance.id)
-                image_id = c.create_image(instance.id, name)
-
-                message = "%s backed up on image %s" % (instance.id, image_id)
-                self.response.out.write(message)
-
-app = webapp2.WSGIApplication([('/', MainPage)],
+app = webapp2.WSGIApplication([('/backup', BackupPage)],
                               debug=True)
---
+++ controllers.py
@@ -0,0 +1,19 @@
+import datetime
+
+import webapp2
+from boto.ec2.connection import EC2Connection
+
+from settings import *
+
+class BackupPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        c = EC2Connection(AWS['key'], AWS['secret'], is_secure=SAFE)
+
+        for reservation in c.get_all_instances():
+            for instance in reservation.instances:
+                name = "%s-%s" % (datetime.date.today(), instance.id)
+                image_id = c.create_image(instance.id, name)
+
+                message = "%s backed up on image %s" % (instance.id, image_id)
+                self.response.out.write(message)
