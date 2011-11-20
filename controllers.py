2bba10e5e106319c46c179e7a81772c3e570263d
Renan Ivo <renanivom@gmail.com>
2011-11-20 16:08:19 -0200

    handle create image exceptions

--- controllers.py
+++ controllers.py
@@ -1,7 +1,7 @@
 import datetime
-
 import webapp2
 from boto.ec2.connection import EC2Connection
+from boto.exception import EC2ResponseError
 
 from settings import *
 
@@ -13,7 +13,11 @@ class BackupPage(webapp2.RequestHandler):
         for reservation in c.get_all_instances():
             for instance in reservation.instances:
                 name = "%s-%s" % (datetime.date.today(), instance.id)
-                image_id = c.create_image(instance.id, name)
 
-                message = "%s backed up on image %s" % (instance.id, image_id)
+                try:
+                    image_id = c.create_image(instance.id, name)
+                    message = "%s backed up on image %s" % (instance.id, image_id)
+                except EC2ResponseError:
+                    message = "Error: %s not backed up" % instance.id
+
                 self.response.out.write(message)
