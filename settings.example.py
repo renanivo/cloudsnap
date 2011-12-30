494b9a2d8d1b19690ca0ae42ff8e598bac37d41b
Renan Ivo <renanivom@gmail.com>
2011-12-30 17:37:07 -0200

    add instructions to fill the sender email address in settings

--- settings.example.py
+++ settings.example.py
@@ -5,7 +5,13 @@ AWS = {
 
 SAFE = True
 
+# From AppEngine documentation (http://goo.gl/AO8lt):
+# For security purposes, the sender address of a message must be the email
+# address of an administrator for the application or any valid email receiving
+# address for the app (see Receiving Mail). The sender can also be the
+# Google Account email address of the current user who is signed in, if the
+# user's account is a Gmail account or is on a domain managed by Google Apps.
 LOGGER = {
-    'sender': 'Cloudsnap Logger<logger@cloudsnap.appspot.com>',
+    'sender': 'Cloudsnap Logger<logger@appid.appspotmail.com>',
     'to': 'Your Email<you@yourdomain.com>',
 }
