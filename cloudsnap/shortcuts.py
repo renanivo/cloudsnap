a915d786b7149dd0e0cb13cd638f3b6b118cc6fd
Renan Ivo <renanivom@gmail.com>
2012-03-14 23:11:21 -0300

    remove function get_all_instances from shortcuts

--- cloudsnap/shortcuts.py
+++ cloudsnap/shortcuts.py
@@ -4,16 +4,6 @@ import jinja2
 from boto.ec2.connection import EC2Connection
 
 
-def get_all_instances(connection):
-    """Return a list of all instances of a EC2Connection"""
-    instances = []
-
-    for reservation in connection.get_all_instances():
-        for instance in reservation.instances:
-            instances.append(instance)
-
-    return instances
-
 def create_instance_backup_name(template, instance):
     env = jinja2.Environment()
     template = env.from_string(template)
--- tests/shortcuts_test.py
+++ tests/shortcuts_test.py
@@ -18,30 +18,6 @@ class ShortcutsTest(unittest.TestCase):
     def tearDown(self):
         self.mox.UnsetStubs()
 
-    def test_get_all_instances_should_return_an_instance_list(self):
-        connection = self.mox.CreateMock(EC2Connection)
-
-        reservation1 = self.mox.CreateMock(Reservation)
-        reservation2 = self.mox.CreateMock(Reservation)
-
-        instance1 = self.mox.CreateMock(Instance)
-        instance2 = self.mox.CreateMock(Instance)
-
-        connection.get_all_instances().AndReturn([reservation1, reservation2])
-
-        reservation1.instances = [instance1]
-        reservation2.instances = [instance2]
-
-        self.mox.ReplayAll()
-
-        instances = get_all_instances(connection)
-
-        self.mox.VerifyAll()
-
-        self.assertEqual(2, len(instances))
-        self.assertIn(instance1, instances)
-        self.assertIn(instance2, instances)
-
     def test_create_instance_backup_name_should_return_a_string_rendered_from_template(self):
         expected = "%s-%s-%s" % (datetime.date.today(), "name", "id")
 
