edfe35fa3922159967aca14c4dd4f15ce71a78ea
Renan Ivo <renan.martins@apontador.com>
2012-08-15 19:15:52 -0300

    allow backups to be filtered by multiple criteria

--- cloudsnap/filters.py
+++ cloudsnap/filters.py
@@ -1,9 +1,15 @@
 def filter_backups_by_tags(backup_list, tags_equal={}, tags_not_equal={}):
-    def callback(item):
+    def equals_filter(item):
         for k, v in tags_equal.items():
             return k in item.tags and item.tags[k] == v
+        return True
 
+    def not_equals_filter(item):
         for k, v in tags_not_equal.items():
             return k not in item.tags or item.tags[k] != v
+        return True
 
-    return filter(callback, backup_list)
+    backup_list = filter(equals_filter, backup_list)
+    backup_list = filter(not_equals_filter, backup_list)
+
+    return backup_list
--- tests/test_filters.py
+++ tests/test_filters.py
@@ -10,9 +10,9 @@ class FiltersTest(unittest.TestCase):
     @patch("boto.ec2.image.Image")
     @patch("boto.ec2.image.Image")
     def setUp(self, backup_mock1, backup_mock2, backup_mock3):
-        backup_mock1.tags = {"foo": "bar"}
-        backup_mock2.tags = {"foo": "foo"}
-        backup_mock3.tags = {"foo": "baz"}
+        backup_mock1.tags = {"foo": "bar", "loren": ""}
+        backup_mock2.tags = {"foo": "fighter"}
+        backup_mock3.tags = {"foo": "bar", "loren": "ipsun"}
 
         self.backup_list = [backup_mock1, backup_mock2, backup_mock3]
 
@@ -20,13 +20,20 @@ class FiltersTest(unittest.TestCase):
         filtered_list = filter_backups_by_tags(self.backup_list,
                                                {"foo": "bar"})
 
-        self.assertEqual(1, len(filtered_list))
+        self.assertEqual(2, len(filtered_list))
         self.assertIn(self.backup_list[0], filtered_list)
 
     def test_should_filter_a_backup_list_by_not_equal_values(self):
         filtered_list = filter_backups_by_tags(self.backup_list,
                                                tags_not_equal={"foo": "bar"})
 
-        self.assertEqual(2, len(filtered_list))
+        self.assertEqual(1, len(filtered_list))
         self.assertIn(self.backup_list[1], filtered_list)
-        self.assertIn(self.backup_list[2], filtered_list)
+
+    def test_should_filter_a_backup_list_by_equal_and_not_equal_values(self):
+        filtered = filter_backups_by_tags(self.backup_list,
+                                          tags_equal={"foo": "bar"},
+                                          tags_not_equal={"loren": "ipsun"})
+
+        self.assertEqual(1, len(filtered))
+        self.assertIn(self.backup_list[0], filtered)
