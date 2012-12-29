6a4247ef56dfa0e07b7f80d30f2ae75314fcbd03
Renan Ivo <renanivom@gmail.com>
2012-12-29 15:52:35 -0200

    add logs to EC2 API calls

--- cloudsnap/ec2adapters.py
+++ cloudsnap/ec2adapters.py
@@ -5,15 +5,18 @@ from boto.ec2.connection import EC2Connection
 
 from settings import AWS, AMI_NAME_TEMPLATE
 from filters import filter_backups_by_tags
+from plus import log_method
 
 
 class EC2Account():
 
     def __init__(self, connection=None):
         if connection is None:
-            self._connection = EC2Connection(AWS['key'],
-                                             AWS['secret'],
-                                             is_secure=AWS['use_safe_connection'])
+            self._connection = EC2Connection(
+                    AWS['key'],
+                    AWS['secret'],
+                    is_secure=AWS['use_safe_connection']
+                    )
         else:
             self._connection = connection
 
@@ -21,7 +24,10 @@ class EC2Account():
         env = jinja2.Environment()
         template = env.from_string(AMI_NAME_TEMPLATE)
 
-        name = instance.tags["Name"] if instance.tags.has_key("Name") else instance.id
+        if "Name" in instance.tags:
+            name = instance.tags["Name"]
+        else:
+            instance.id
 
         return template.render({
             "today": datetime.date.today(),
@@ -29,6 +35,7 @@ class EC2Account():
             "instance_id": instance.id,
             })
 
+    @log_method
     def get_instances(self):
         """Return a list of all EC2 instances of the account"""
         instances = []
@@ -39,22 +46,28 @@ class EC2Account():
 
         return instances
 
+    @log_method
     def backup_instance(self, instance):
         """Create a backup (AMI) of a given instance"""
-        image_id = self._connection.create_image(instance.id,
-                                               self._create_AMI_name(instance))
+        image_id = self._connection.create_image(
+                    instance.id,
+                    self._create_AMI_name(instance)
+                    )
         self._connection.create_tags([image_id],
                                      {'instance': instance.id,
                                       'created_at': datetime.date.today(),
-                                      'created_by': 'cloudsnap'})
+                                      'created_by': 'cloudsnap',
+                                      })
         return image_id
 
+    @log_method
     def get_backups(self):
         """Return a list of all backups (AMIs) created by it"""
         backups = self._connection.get_all_images()
 
         return filter_backups_by_tags(backups, {"created_by": "cloudsnap"})
 
+    @log_method
     def delete_backup(self, image):
         """Delete a backup (AMI)"""
         self._connection.deregister_image(image.id, True)
---
+++ cloudsnap/plus.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+import logging
+import time
+import functools
+import datetime
+
+
+class LogFunctionDecorator(object):
+    """decorate a function to log its execution"""
+
+    def __init__(self, func):
+        self.func = func
+        self.elapsed_time = 0
+
+    def __call__(self, *args, **kwargs):
+        start_time = time.time()
+        result = self.func(*args, **kwargs)
+        self.elapsed_time = time.time() - start_time
+
+        message = u"""
+        %(now)s: %(name)s executed in %(elapsed).6f secs with %(params)s
+        """
+
+        logging.info(message % self._get_log_data(args, kwargs))
+        return result
+
+    def _get_log_data(self, params, kw_params):
+        return {u"name": self.func.__name__,
+                u"elapsed": self.elapsed_time,
+                u"now": datetime.datetime.now(),
+                u"params": "args: %s and kwargs: %s" % (params, kw_params),
+                }
+
+
+class LogMethodDecorator(LogFunctionDecorator):
+    """decorate a method to log its execution"""
+    def __init__(self, func):
+        super(LogMethodDecorator, self).__init__(func)
+
+    def _get_log_data(self, params, kw_params):
+        try:
+            class_name = params[0].__class__.__name__
+        except AttributeError:
+            class_name = params[0].__name__
+
+        data = super(LogMethodDecorator, self)._get_log_data(params, kw_params)
+        data[u"name"] = "%s.%s" % (class_name, self.func.__name__)
+
+        return data
+
+
+def log_function(func):
+    """
+    decorate a function to log its execution keeping its __name__ and __doc__
+    """
+    decorated = LogFunctionDecorator(func)
+
+    @functools.wraps(func)
+    def helper(*args, **kwargs):
+        return decorated(*args, **kwargs)
+
+    return helper
+
+
+def log_method(func):
+    """
+    decorate a method to log its execution keeping its __name__ and __doc__
+    """
+    decorated = LogMethodDecorator(func)
+
+    @functools.wraps(func)
+    def helper(*args, **kwargs):
+        return decorated(*args, **kwargs)
+
+    return helper
